67baded4f40b79fdbdbe9a170b16e844e9dfe726
Carlos Garibay <garibaycarlos@live.com>
2026-01-10 19:05:32 -0600

    Add EF Core, Villa model, and initial migrations
    Integrated Entity Framework Core with SQL Server support, added ApplicationDbContext and Villa model, and created initial migrations to set up the Villa table. Updated DI registration, connection string, and project dependencies accordingly. Removed unused Controllers folder entry from project file.

---
+++ src/RoyalVilla/RoyalVilla_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoyalVilla_API.Models;
+
+namespace RoyalVilla_API.Data;
+
+public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Villa> Villa { get; set; }
+}
